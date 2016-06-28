1ca8e1fdcd2454e4f5cadf5b966202c4057083fa
SubEthaSensOMatic <ajid.welzel@gmail.com>
2016-06-28 15:29:26 +0200

    ExtendedValueConverter

---
+++ Converter/ExtendedValueConverter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using TypeUtils.Services;
+using TypeUtils.Services.Impl;
+
+namespace WpfUtils.Converter
+{
+    public class ExtendedValueConverter : IValueConverter
+    {
+        private ITypeConverter _converter;
+
+        public ExtendedValueConverter()
+        {
+            // Try DI
+            try
+            {
+                if (ServiceLocator.Current != null)
+                    _converter = ServiceLocator.Current.GetInstance<ITypeConverter>();
+            }
+            catch (Exception) {}
+            
+            // Default
+            if (_converter == null)
+                _converter = TypeConverter.Current;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return _converter.convert(value, targetType);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return _converter.convert(value, targetType);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("TypeUtils")]
+[assembly: AssemblyDescription("Useful WPF utilities")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ajid Welzel")]
+[assembly: AssemblyProduct("WpfUtils")]
+[assembly: AssemblyCopyright("Copyright © 2016 Ajid Welzel")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("74bf7908-841c-49d8-b40c-8a293fb4e769")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
